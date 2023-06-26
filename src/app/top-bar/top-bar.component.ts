59a9c8382491a4f863964a57936845cc9fcad3f6
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-06-26 15:59:30 +0100

    Top-bar create

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss']
+})
+
+/**
+* This component will render the top bar of the app
+* @export
+* @class TopBarComponent
+* @implements {OnInit}
+* @example <app-top-bar></app-top-bar>
+* @see Router
+*/
+
+export class TopBarComponent {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  toMovies(): void {
+    this.router.navigate(['movies']);
+  }
+
+  toProfile(): void {
+    this.router.navigate(['profile']);
+  }
+
+  logOut(): void {
+    this.router.navigate(['welcome']);
+    localStorage.clear();
+  }
+}
